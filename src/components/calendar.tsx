3dee3ee4e31d45e4c0b4194d62de6f1ab5dce04a
Denise Calsavara <dedacpo@gmail.com>
2023-04-08 11:10:44 -0300

    feat: adding interative month and year selection

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -4,42 +4,83 @@ import { getDaysInMonth, startOfMonth } from "date-fns";
 import { useEffect, useState } from "react";
 
 export default function Calendar() {
-
-  const days = [];
-
   const weekDays = [0, 1, 2, 3, 4, 5, 6];
 
   const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-  const [currentDay, setCurrentDay] = useState<Date>(new Date())
-  const [selectedMonth, setSelectedMonth] = useState<number>(currentDay.getMonth())
-  const [daysInMonth, setDaysInMonth] = useState<number>(getDaysInMonth(currentDay));
-  const [firstDayOfMonth, setFirstDayOfMonth] = useState<number>(startOfMonth(currentDay).getDay())
-  const [emptyDays, setEmptyDays] = useState<number>(firstDayOfMonth > 0 ? firstDayOfMonth - 1 : firstDayOfMonth)
+  const [currentDay, setCurrentDay] = useState<Date>(new Date());
+  const [selectedMonth, setSelectedMonth] = useState<number>(
+    currentDay.getMonth()
+  );
+  const [selectedYear, setSelectedYear] = useState<number>(
+    currentDay.getFullYear()
+  );
+  const [daysInMonth, setDaysInMonth] = useState<number>(
+    getDaysInMonth(currentDay)
+  );
+  const [firstDayOfMonth, setFirstDayOfMonth] = useState<number>(
+    startOfMonth(currentDay).getDay()
+  );
+  const [emptyDays, setEmptyDays] = useState<number>(
+    firstDayOfMonth > 0 ? firstDayOfMonth - 1 : firstDayOfMonth
+  );
+  const [days, setDays] = useState<JSX.Element[]>();
 
-  for (let i = 0; i <= daysInMonth + emptyDays; i++) {
-    days.push(
-      <div key={`day-${i - firstDayOfMonth + 1}`}>
-        {i >= firstDayOfMonth && <span>{i - firstDayOfMonth + 1}</span>}
-      </div>
-    );
-  }
+  useEffect(() => {
+    const newDate = new Date(selectedYear, selectedMonth);
+    console.log("newDate", newDate, selectedMonth, selectedYear);
+    setCurrentDay(newDate);
+  }, [selectedMonth, selectedYear]);
+
+  useEffect(() => {
+    setDaysInMonth(getDaysInMonth(currentDay));
+    setFirstDayOfMonth(startOfMonth(currentDay).getDay());
+  }, [currentDay]);
+
+  useEffect(() => {
+    setEmptyDays(firstDayOfMonth > 0 ? firstDayOfMonth - 1 : firstDayOfMonth);
+  }, [firstDayOfMonth]);
+
+  useEffect(() => {
+    const daysElements = [];
+    console.log(daysInMonth, emptyDays);
+    for (let i = 0; i <= daysInMonth + emptyDays; i++) {
+      daysElements.push(
+        <div key={`day-${i - firstDayOfMonth + 1}`}>
+          {i >= firstDayOfMonth && <span>{i - firstDayOfMonth + 1}</span>}
+        </div>
+      );
+    }
+    setDays(daysElements);
+  }, [emptyDays]);
 
   return (
     <>
       <div>
-        <select value={selectedMonth} onChange={(e) => setSelectedMonth(Number(e?.target?.value))}>
-          {months.map(item => {
+        <select
+          value={selectedMonth}
+          onChange={(e) => setSelectedMonth(Number(e?.target?.value))}
+        >
+          {months.map((item) => {
             return (
-              <option key={`month-${Month[item]}`} value={item}>{Month[item]}</option>
-            )
+              <option key={`month-${Month[item]}`} value={item}>
+                {Month[item]}
+              </option>
+            );
           })}
-        </select>       
+        </select>
+        <label htmlFor="year">Year</label>
+        <br />
+        <input
+          type="number"
+          value={selectedYear}
+          onChange={(e) => setSelectedYear(Number(e.target.value ?? undefined))}
+        />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-7">
         {weekDays.map((item) => {
-          return <div  key={`weekDay-${WeekDay[item]}`}>{WeekDay[item]}</div>;
+          return <div key={`weekDay-${WeekDay[item]}`}>{WeekDay[item]}</div>;
         })}
-        {days.map((item) => item)}
+        {days?.map((item) => item)}
       </div>
     </>
   );
